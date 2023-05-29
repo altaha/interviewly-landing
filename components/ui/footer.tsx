2314e1a7d87749c0a67a71bc124d4f9de5c98095
Ahmed Altaha <ahmedsalam.2008@gmail.com>
2023-05-29 13:22:56 -0400

    Bring back Footer with links to Terms, Privacy, and social

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,9 +6,10 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
         {/* Top area: Blocks */}
+        {/*
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
 
-          {/* 1st block */}
+          {/* 1st block * /}
           <div className="sm:col-span-12 lg:col-span-3">
             <div className="mb-2">
               <Logo />
@@ -18,7 +19,7 @@ export default function Footer() {
             </div>
           </div>
 
-          {/* 2nd block */}
+          {/* 2nd block * /}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
             <h6 className="text-gray-800 font-medium mb-2">Products</h6>
             <ul className="text-sm">
@@ -40,7 +41,7 @@ export default function Footer() {
             </ul>
           </div>
 
-          {/* 3rd block */}
+          {/* 3rd block * /}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
             <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
             <ul className="text-sm">
@@ -62,7 +63,7 @@ export default function Footer() {
             </ul>
           </div>
 
-          {/* 4th block */}
+          {/* 4th block * /}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
             <h6 className="text-gray-800 font-medium mb-2">Company</h6>
             <ul className="text-sm">
@@ -84,7 +85,7 @@ export default function Footer() {
             </ul>
           </div>
 
-          {/* 5th block */}
+          {/* 5th block * /}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
             <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
             <p className="text-sm text-gray-600 mb-4">Get the latest news and articles to your inbox every month.</p>
@@ -101,27 +102,43 @@ export default function Footer() {
                       </svg>
                     </button>
                   </div>
-                  {/* Success message */}
-                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
+                  {/* Success message * /}
+                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> * /}
                 </div>
               </div>
             </form>
           </div>
 
         </div>
+        */}
 
         {/* Bottom area */}
-        <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
+        <div className="sm:flex sm:items-center sm:justify-between py-4 sm:py-8 border-t border-gray-200">
+          {/* Terms and Privacy policy links */}
+          <div>
+            <div className='flex space-x-4 items-center'>
+              <Logo />
+              <a href="#0" className="text-sm text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">
+                Terms
+              </a>
+              <a href="#0" className="text-sm text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">
+                Privacy Policy
+              </a>
+            </div>
+            {/* Copyrights note */}
+            <div className="text-sm text-gray-600 mt-1">&copy; Interviewly.ai. All rights reserved.</div>
+          </div>
 
           {/* Social as */}
-          <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
+          <ul className="flex mt-4 sm:mt-0">
             <li>
-              <a href="#0" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
+              <a href="https://twitter.com/InterviewlyAI" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                   <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
                 </svg>
               </a>
             </li>
+            {/*
             <li className="ml-4">
               <a href="#0" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Github">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
@@ -136,10 +153,8 @@ export default function Footer() {
                 </svg>
               </a>
             </li>
+            */}
           </ul>
-
-          {/* Copyrights note */}
-          <div className="text-sm text-gray-600 mr-4">&copy; Cruip.com. All rights reserved.</div>
 
         </div>
 
