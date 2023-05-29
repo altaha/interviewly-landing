a56ba7e8b9c2c9f40c69a7592afe979e333b26be
Ahmed Altaha <ahmedsalam.2008@gmail.com>
2023-05-29 11:53:55 -0400

    SEO optimizations: Add opengraph tags to display on social media

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,14 +1,14 @@
-export const metadata = {
-  title: 'Home | Interviewly.ai',
-  description: 'Interviewly.ai is your AI Interview Coach. Practice your interviews with an AI mock Interviewer.',
-}
-
+import { Metadata } from 'next'
 import  { Analytics }  from   '@vercel/analytics/react' ;
 import Hero from '@/components/hero'
 import Features from '@/components/features'
 import FeaturesBlocks from '@/components/features-blocks'
 import Testimonials from '@/components/testimonials'
 import Newsletter from '@/components/newsletter'
+
+export const metadata: Metadata = {
+  title: "Home | Interviewly.ai",
+}
 
 export default function Home() {
   return (
